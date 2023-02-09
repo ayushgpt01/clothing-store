5606e469fd38b827fcabf3e9f32c3bd9a6e1fcf3
ayushgpt01 <ayushgpt043@gmail.com>
2023-02-09 16:03:13 +0530

    Added firebase database fetching and refactored product to categories

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,18 +1,18 @@
 import { Fragment, useContext } from "react";
-import { ProductsContext } from "../../context/products.context";
+import { CategoriesContext } from "../../context/categories.context";
 import ProductCard from "../../components/product-card/product-card.component";
 
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const { products } = useContext(ProductsContext);
+  const { categoriesMap } = useContext(CategoriesContext);
   return (
     <Fragment>
-      {Object.keys(products).map((title) => (
+      {Object.keys(categoriesMap).map((title) => (
         <Fragment key={title}>
           <h2>{title}</h2>
           <div className="products-container">
-            {products[title].map((product) => (
+            {categoriesMap[title].map((product) => (
               <ProductCard product={product} key={product.id} />
             ))}
           </div>
