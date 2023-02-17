fea2db0c8e44b2a6907bf053f3b6d31a66142d1e
ayushgpt01 <ayushgpt043@gmail.com>
2023-02-17 23:31:44 +0530

    Added redux-persist

--- src/index.js
+++ src/index.js
@@ -1,25 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
+
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import { store, persistor } from "./store/store";
+
 import "./index.scss";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
--- src/store/store.js
+++ src/store/store.js
@@ -1,16 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./slices/userSlice";
 import categoriesReducer from "./slices/categoriesSlice";
 import cartReducer from "./slices/cartSlice";
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedCategories = persistReducer(persistConfig, categoriesReducer);
+const persistedCart = persistReducer(persistConfig, cartReducer);
+
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    categories: categoriesReducer,
-    cart: cartReducer,
+    categories: persistedCategories,
+    cart: persistedCart,
   },
+  devTools: process.env.NODE_ENV !== "production",
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+export const persistor = persistStore(store);
