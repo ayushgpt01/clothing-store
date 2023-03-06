bb008fc1db49b208077d0d13021b5678a00fbc93
ayushgpt01 <ayushgpt043@gmail.com>
2023-03-06 19:39:21 +0530

    Fixed rootElement bug with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,8 @@ import { stripePromise } from "./utils/stripe/stripe.utils";
 import App from "./App";
 
 import "./index.scss";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root")!;
+const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
